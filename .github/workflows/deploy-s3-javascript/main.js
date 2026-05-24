cd5d1806497a88f9493ff90cdc6df69206d72861
yimoun <nelsonyimou@gmail.com>
2026-05-24 05:05:03 -0400

    fix: fixed error in deploy workflow

--- .github/workflows/deploy-s3-javascript/main.js
+++ .github/workflows/deploy-s3-javascript/main.js
@@ -1,6 +1,6 @@
-core = require('@actions/core');
-github = require('@actions/github');
-exec = require('@actions/exec');
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 
 function run () {
